6c175b57700c90ef01b54a8d9a4c14b8cc01e5f4
ING <safernandez1@miuandes.cl>
2018-04-03 16:18:07 -0300

    First commit for april 3 lab

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cliente : Persona
+    {
+        string licencia_conducir;
+        string vehiculo_licencia;
+        string empresa;
+        string organizacion;
+        string institucion;
+        public Cliente(string licencia_conducir, string vehiculo_licencia, string empresa, string organizacion, string institucion) : base(licencia_conducir, vehiculo_licencia)
+        {
+            this.licencia_conducir = licencia_conducir;
+            this.vehiculo_licencia = vehiculo_licencia;
+            this.empresa = empresa;
+            this.organizacion = organizacion;
+            this.institucion = institucion;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Persona : Vehiculo
+    {
+        String tipo_auto;
+        String licencia_conducir;
+        String vehiculo_licencia;
+
+        public Persona(string tipo_auto, string licencia_conducir, string vehiculo_licencia) : base(tipo_auto)
+        {
+            this.tipo_auto = tipo_auto;
+            this.Licencia_conducir = licencia_conducir;
+            this.vehiculo_licencia = vehiculo_licencia;
+        }
+
+        public string Licencia_conducir
+        {
+            get => licencia_conducir;
+            set
+            {
+                if (vehiculo_licencia == tipo_auto)
+                {
+                    licencia_conducir = tipo_auto;
+                }
+            }
+        }
+    }
+}
